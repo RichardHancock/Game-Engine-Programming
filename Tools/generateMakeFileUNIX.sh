73f739685d7c642d46ddb836cb91f8b68e4c6edf
Richard Hancock <me@richardhancock.co.uk>
2017-03-04 16:03:13 +0000

    Added Linux Dependencies

--- Tools/generateMakeFileUNIX.sh
+++ Tools/generateMakeFileUNIX.sh
@@ -1,6 +1,6 @@
 MKGEN_SRC_DIR="../Engine"
 MKGEN_COMPILER="g++"
-MKGEN_FLAGS="-std=c++11 -std=gnu++11 -lGLEW -lGLU -lGL -lSDL2 -lSDL2_mixer -lSDL2_image -lSDL2_ttf -lpugixml -lLinearMath -lBulletCollision -lBulletDynamics -lassimp \$(shell sdl2-config --cflags) -I/usr/include/SDL2 -I/usr/include/bullet"
+MKGEN_FLAGS="-std=c++11 -std=gnu++11 -lGLEW -lGLU -lGL -lSDL2 -lSDL2_mixer -lSDL2_image -lSDL2_ttf -lpugixml -lLinearMath -lBulletCollision -lBulletDynamics -lassimp -lboost_system -lboost_random -lsioclient \$(shell sdl2-config --cflags) -I/usr/include/SDL2 -I/usr/include/bullet"
 MKGEN_DEBUG_FLAGS="-g -Wall -pedantic"
 MKGEN_RELEASE_FLAGS="-O2 -Wall -pedantic"
 MKGEN_OUTPUT_PATH="../bin/"
