9eef0191a96ef28759d8908e2895566c7cf642d2
Richard Hancock <me@richardhancock.co.uk>
2017-03-08 05:17:01 +0000

    Reworked build script to support custom resource dirs. #29

--- Tools/generateMakeFileWIN.bat
+++ Tools/generateMakeFileWIN.bat
@@ -7,5 +7,6 @@ SET mkgen_outputPath="../bin/"
 SET mkgen_outputFile="Engine"
 SET mkgen_resourceFolderName="resources"
 SET mkgen_resourceFolderPath="../Engine/"
+SET mkgen_resourceFolderOutputPath="../bin/"
 
-call python makeFileGenerator.py %mkgen_srcDir% %mkgen_compiler% %mkgen_flags% %mkgen_debugFlags% %mkgen_releaseFlags% %mkgen_outputPath% %mkgen_outputFile% %mkgen_resourceFolderName% %mkgen_resourceFolderPath%
+call python makeFileGenerator.py %mkgen_srcDir% %mkgen_compiler% %mkgen_flags% %mkgen_debugFlags% %mkgen_releaseFlags% %mkgen_outputPath% %mkgen_outputFile% %mkgen_resourceFolderName% %mkgen_resourceFolderPath% %mkgen_resourceFolderOutputPath%
